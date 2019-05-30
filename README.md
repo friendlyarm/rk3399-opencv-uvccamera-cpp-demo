32cfd58c747b2fdaa7a7d061722fc6a0bf16d8e2
Lawrence-Tang <tangzongsheng@gmail.com>
2019-05-30 14:59:33 +0800

    first time commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## **opencv-uvccamera-cpp-demo**
+
+Build
+------------
+```
+git clone https://github.com/friendlyarm/opencv-uvccamera-cpp-demo.git
+cd opencv-uvccamera-cpp-demo
+mkdir build
+cd build
+cmake ..
+make
+```
+
+Run
+------------
+```
+su pi
+export DISPLAY=:0.0
+. setqt5env
+./opencamera
+```
---
+++ opencamera.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
+
+int main() 
+{ 
+	VideoCapture capture("v4l2src device=/dev/video8 io-mode=4 ! videoconvert ! video/x-raw,format=NV12,width=800,height=448,framerate=30/1 ! videoconvert ! appsink",cv::CAP_GSTREAMER);
+	if (!capture.isOpened()) {
+		printf("Fail to open camera.\n");
+		return 1;
+	}
+	namedWindow("win", 1);
+	while (1) { 
+		Mat frame;
+		capture >> frame;
+		if (frame.empty()) {
+			printf("Fail to read frame.\n");
+			break;
+		}
+		imshow("win",frame);
+		if (waitKey(30) & 0xFF == 'q') 
+			break;
+	} 
+	capture.release();
+	return 0; 
+}
+
+
+
+
